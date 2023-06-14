4540232d4da33b7f7210061070b53fac62a5e1ca
Alex Zhang <alexzhangyaotian@gmail.com>
2023-06-14 22:30:22 +0800

    :zap: chore:router & vuex 整合

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,11 @@ const handleMyClick = ()=>{
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div @click="$store.commit('add')">
+    {{ $store.state.counter }}
   </div>
-  <HelloWorld msg="Vite + Vue" @myclick="handleMyClick" ref="hw"/>
+  <!-- <HelloWorld msg="Vite + Vue" @myclick="handleMyClick" ref="hw"/> -->
+  <router-view></router-view>
 </template>
 
 <style scoped>
---
+++ src/views/home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <HelloWorld></HelloWorld>
+        <p>router</p>
+    </div>
+</template>
+
+<script setup>
+import HelloWorld from '../components/HelloWorld.vue';
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
